4b2ccbd0a6fe0fee502c14469ec9bbf300738dc2
1576520768@qq.com <1576520768@qq.com>
2019-03-28 00:12:53 +0800

    feat(add): add ch15

---
+++ src/ch15/series/my_series.go
@@ -0,0 +1,25 @@
+package series
+
+import "fmt"
+
+func init() {
+	fmt.Println("init1")
+}
+
+func init() {
+	fmt.Println("init2")
+}
+
+// Square 求平方
+func Square(n int) int {
+	return n * n
+}
+
+// GetFibonacciSerie 斐波那契数列
+func GetFibonacciSerie(n int) []int {
+	ret := []int{1, 1}
+	for i := 2; i < n; i++ {
+		ret = append(ret, ret[i-2]+ret[i-1])
+	}
+	return ret
+}
