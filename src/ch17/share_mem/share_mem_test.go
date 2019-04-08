dab83b4d856aebc49f4715623d28d3743ff10a11
wangjinxu <1576520768@qq.com>
2019-04-08 17:39:56 +0800

    feat(add): add ch17

--- src/ch17/share_mem/share_mem_test.go
+++ src/ch17/share_mem/share_mem_test.go
@@ -1,6 +1,7 @@
 package share_mem
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -13,5 +14,40 @@ func TestCounter(t *testing.T) {
 		}()
 	}
 	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 	t.Logf("counter = %d", counter)
 }
